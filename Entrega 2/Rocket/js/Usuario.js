dadbc80049c8c4d4fdc8daf5ca6a2372edfcdfb7
David Gonzalez <crisdavidgonzalezc@gmail.com>
2021-04-19 18:49:38 -0500

    Añadida carpeta Javascript

---
+++ Entrega 2/Rocket/js/Usuario.js
@@ -0,0 +1,11 @@
+export class Usuario {
+    constructor(nombre, apellido, nomUsuario, contrasena, Id, usuarioPay, email){
+        this.nombre=nombre;
+        this.apellido=apellido;
+        this.nomUsuario=nomUsuario;
+        this.contrasena=contrasena;
+        this.Id=Id;
+        this.usuarioPay=usuarioPay;
+        this.email=email;
+    }
+}
