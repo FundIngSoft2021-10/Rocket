026b7b62c3a28349e8e654f05147594277b249e3
Julian Rizo <jr.rizo.o.jr@gmail.com>
2021-04-19 17:33:05 -0500

    prueba js usuario

--- Entrega 2/Rocket/Js/Rocket.js
+++ Entrega 2/Rocket/Js/Rocket.js
@@ -1,26 +1,29 @@
 
-class Rocket{
+class Rocket {
     constructor (){
-        var listaUsuarios = new Array();
-       // var controladorTrans = new ControladorTransaccion();
+        this.listaUsuarios = new Array();
+        // var listaUsuarios = new Array();
+        // var controladorTrans = new ControladorTransaccion();
     }
+
     crearUsuario(){
         var Aux = new Usuario("Julian","Gonorreas","jr.rizo","aguacate00", 1 ,"soypobre1230", "jr.rizo.o.jr@gmail.com");
-        listaUsuarios.push(Aux);
+        rocket.listaUsuarios.push(Aux);
         alert("se ha creado");
+        
     }
+
     get getUsuarios(){
         return this.listaUsuarios;
     }
-     saludar()
-     {
-         alert("ing.soft nos va a vomitar en la cara");
-     }   
+
+    saludar()
+    {
+        alert("ing.soft nos va a vomitar en la cara");
+    }   
     
 }
 
 rocket = new Rocket();
-
-
-
-
+rocket.crearUsuario();
+console.log(rocket.listaUsuarios[0].nombre);
